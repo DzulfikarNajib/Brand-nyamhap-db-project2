f19d07cfa35b4ca295eacbb7dbd963db099c3af4
Dzulfikar Najib (Cent) <dzulfikarnajib315@gmail.com>
2025-12-04 12:43:17 +0700

    Add files via upload

---
+++ NYAMHAP/footer.php
@@ -0,0 +1,10 @@
+<?php
+?>
+        </main>
+    <footer class="mt-5 p-3 bg-light text-center">
+        <p>&copy; <?= date('Y') ?> NYAMHAP. PROJECT BASIS DATA.</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
