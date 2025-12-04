9be69c25b4c83d1592ac0ba67b74597f00ad781a
Dzulfikar Najib (Cent) <dzulfikarnajib315@gmail.com>
2025-12-04 11:02:42 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,19 +1,21 @@
 <?php
-
+// Konfigurasi koneksi database
 $host     = 'localhost';
-$dbname   = 'NYAMHAP';
+$dbname   = 'NYAMHAP';   // Nama database yang ada di DBeaver/ToolsSQL lainnya
 $user     = 'postgres';
-$password = 'Najib2202';
+$password = 'isi_password'; // Ganti dengan password PostgreSQL kamu
 
+// Membuat koneksi
 $conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
 
+// Validasi koneksi
 if (!$conn) {
-    die(" Koneksi database gagal: " . pg_last_error());
+    die("Koneksi database gagal: " . pg_last_error());
 }
 
+// Set schema default
 pg_query($conn, "SET search_path TO public");
 
-
+// Variabel global koneksi
 $koneksi = $conn;
-
 ?>
